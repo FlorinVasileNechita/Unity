bdc9c2c6780748066dfe67dc6c77b8e8a0154a15
TashaGospel <nhanhnhanh17399@gmail.com>
2015-07-10 16:21:46 +0700

    Hello World!

---
+++ Clony Bird/Assets/BirdMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+	public int health=15;
+
+	// Use this for initialization
+	void Start () {
+		DamagePlayer (20);
+	}
+
+	void DamagePlayer(int damage) {
+		health -= damage;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0) {
+			Destroy(gameObject, 3f);
+			Debug.Log ("The player has died!");
+		}
+	}
+}
