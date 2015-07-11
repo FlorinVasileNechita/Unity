da4eaf480dd6a38b5a5287c5d213dd7bc695f07c
TashaGospel <nhanhnhanh17399@gmail.com>
2015-07-11 20:38:41 +0700

    More scripts

---
+++ ClonyBird/Assets/BirdMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+	public Vector3 gravity;
+	public Vector3 flapVelocity;
+	public float forwardSpeed;
+	public float maxSpeed;
+	Vector3 velocity = Vector3.zero;
+
+	bool didFlap = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	//Graphics and Inputs go here
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+			didFlap = true;
+		}
+	}
+	
+	// Physics go here
+	void FixedUpdate () {
+		velocity.x = forwardSpeed;
+		velocity += gravity * Time.deltaTime;
+
+		if (didFlap) {
+			didFlap = false;
+			velocity.y = (velocity.y < 0)? 0:velocity.y;
+			velocity += flapVelocity;
+		}
+
+		velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+		transform.position += velocity * Time.deltaTime;
+
+		float angle = 0;
+		if (velocity.y < 0) {
+			angle = Mathf.Lerp(0, -90, -velocity.y / maxSpeed);
+		}
+		transform.rotation = Quaternion.Euler (0, 0, angle);
+	}
+}
---
+++ ClonyBird/Assets/CameraTracksPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTracksPlayer : MonoBehaviour {
+	Transform player;
+	float offsetX;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+
+		offsetX = transform.position.x - player.position.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null){
+			Vector3 pos = transform.position;
+			pos.x = player.position.x + offsetX;
+			transform.position = pos;
+		}
+	}
+}
