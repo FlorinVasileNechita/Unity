b7cde3e0f38f701dcba78a202bc0c21dc5ba919b
TashaGospel <nhanhnhanh17399@gmail.com>
2015-07-12 00:06:32 +0700

    Random bird skin

---
+++ ClonyBird/Assets/BirdReskin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdReskin : MonoBehaviour {
+
+	public Sprite[] birdSprites;
+	int spriteNum;
+
+	// Use this for initialization
+	void Start () {	
+		spriteNum = Random.Range (0,3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine(Reskin());
+	}
+
+	IEnumerator Reskin() {
+		while (true)
+		for (int i=spriteNum*12; i < spriteNum*12+12; i++) { //numOfFrames(4) * numOfSprites(3)
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = birdSprites[i/4];
+			yield return 0;
+		}
+	}
+}
