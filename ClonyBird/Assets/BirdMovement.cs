009d4d9cdf9e0aef49eb8d8184fb85cbbd8523dd
TashaGospel <nhanhnhanh17399@gmail.com>
2015-07-13 14:40:22 +0700

    Bird fixed

--- ClonyBird/Assets/BirdMovement.cs
+++ ClonyBird/Assets/BirdMovement.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 
 public class BirdMovement : MonoBehaviour {
-	public Vector3 flapVelocity;
-	public float forwardSpeed;
-	public float maxSpeed;
 	Vector3 velocity = Vector3.zero;
+	float flapSpeed = 100f;
+	float forwardSpeed = 1f;
+
+	Rigidbody2D bird;
+	Animator animator;
 
 	bool didFlap = false;
 
 	// Use this for initialization
 	void Start () {
+		bird = GetComponent<Rigidbody2D>();
+		animator = GetComponentInChildren<Animator>();
 
 	}
 
@@ -23,23 +27,45 @@ public class BirdMovement : MonoBehaviour {
 	
 	// Physics go here
 	void FixedUpdate () {
-		velocity.x = forwardSpeed;
+		bird.AddForce(Vector2.right * forwardSpeed);
 
 		if (didFlap) {
 			didFlap = false;
-			velocity.y = (velocity.y < 0)? 0:velocity.y;
-			velocity += flapVelocity;
+
+			animator.SetTrigger("DoFlap");
+
+			if (bird.velocity.y < 0) 
+				bird.AddForce(Vector2.up * (-bird.velocity.y));
+			bird.AddForce(Vector2.up * flapSpeed);
 		}
-
-		velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-
-		GetComponent<Rigidbody2D>().AddForce(velocity);
-
 
 		float angle = 0;
-		if (velocity.y < 0) {
-			angle = Mathf.Lerp(0, -90, -velocity.y / maxSpeed);
-		}
-		transform.rotation = Quaternion.Euler (0, 0, angle);
+		if (bird.velocity.y < 0.25f) 
+			angle = Mathf.Lerp(0, -90, -(bird.velocity.y+0.25f) / 2f);
+		transform.rotation = Quaternion.Euler(0,0,angle);
+	
+
+
+
+
+//		velocity.x = forwardSpeed;
+//		velocity.y = gravity * Time.deltaTime;
+//
+//		if (didFlap) {
+//			didFlap = false;
+//			velocity.y = (velocity.y < 0)? 0:velocity.y;
+//			velocity += flapVelocity;
+//		}
+//
+//		velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+//
+//		transfrom.position += velocity * Time.deltaTime;
+//
+//
+//		float angle = 0;
+//		if (velocity.y < 0) {
+//			angle = Mathf.Lerp(0, -90, -velocity.y / maxSpeed);
+//		}
+//		transform.rotation = Quaternion.Euler (0, 0, angle);
 	}
 }
